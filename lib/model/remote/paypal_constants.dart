dcb40c05d4abb2b7be055359725d87e4e16e5474
ahmed emara <emara8028@gmail.com>
2024-07-10 04:05:09 +0300

    add paypal first step

---
+++ lib/model/remote/paypal_constants.dart
@@ -0,0 +1,5 @@
+class PaypalConstants
+{
+  static const String paypalClientId = 'AXGMHbYoP0bsESH9aneqHmNcNIzdTtgE7OLKodqvg_eu8G7xhwzFoXu5lG8NfxSoKv5jJ1-rxl6f1r7h';
+  static const String secretKey = 'ELAXxRrD0ZEtbr1_9N7yV2hv--0FsAkADC04yxxQK23QOuzXUbcTGrUCu2J_b-OJ63PGCcWVR3Ku26Hu';
+}
